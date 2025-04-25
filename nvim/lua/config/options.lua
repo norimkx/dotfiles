e1d2361d2e59f73e6aacbc6a137d29345e466dcc
norimkx <norimkx@gmail.com>
2025-04-26 03:59:46 +0900

    first commit

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,26 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+if vim.fn.has("wsl") == 1 then
+  if vim.fn.executable("wl-copy") == 0 then
+    print("wl-clipboard not found, clipboard integration won't work")
+  else
+    vim.g.clipboard = {
+      name = "wl-clipboard (wsl)",
+      copy = {
+        ["+"] = "wl-copy --foreground --type text/plain",
+        ["*"] = "wl-copy --foreground --primary --type text/plain",
+      },
+      paste = {
+        ["+"] = function()
+          return vim.fn.systemlist("wl-paste --no-newline|sed -e 's/\r$//'", { "" }, 1)
+        end,
+        ["*"] = function()
+          return vim.fn.systemlist("wl-paste --primary --no-newline|sed -e 's/\r$//'", { "" }, 1)
+        end,
+      },
+      cache_enabled = true,
+    }
+  end
+end
