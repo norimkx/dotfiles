78b77fb2947a5004d71824e1a8b9755da5124bf1
norimkx <norimkx@gmail.com>
2025-04-28 13:31:01 +0900

    feat: add keymaps to insert datetime and better paste

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("v", "p", "pgvy")
+
+vim.keymap.set(
+  "n",
+  "<leader>cid",
+  "i<C-r>=strftime('%Y-%m-%d %H:%M:%S')<CR><Esc>",
+  { desc = "Insert Datetime", silent = true }
+)
+
+vim.keymap.set(
+  "v",
+  "<leader>cid",
+  "c<C-r>=strftime('%Y-%m-%d %H:%M:%S')<CR><Esc>",
+  { desc = "Insert Datetime", silent = true }
+)
