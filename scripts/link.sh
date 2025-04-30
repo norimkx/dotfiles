3f44f47e3dad088a6f93b8075dfb2a29eeef6058
norimkx <norimkx@gmail.com>
2025-04-30 22:50:25 +0900

    fix: link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# シンボリックリンクを作成する設定 (リンク先 => dotfiles からの相対パス)
+# シンボリックリンクを作成する設定 (dotfiles からの相対パス = リンクを作成するディレクトリ)
 declare -A links=(
-  ["$HOME/.gitconfig"]=".gitconfig"
-  ["$HOME/.config/nvim"]="nvim"
+  ["nvim"]="$HOME/.config"
+  [".bashrc"]="$HOME"
+  [".gitconfig"]="$HOME"
 )
 
 # スクリプト自身の絶対パスを取得
@@ -12,40 +13,32 @@ SCRIPT_PATH=$(realpath "$0")
 # スクリプトが存在するディレクトリの絶対パスを取得
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-# dotfiles ルートディレクトリをスクリプトの親ディレクトリと仮定
+# dotfiles ディレクトリの絶対パスを取得
 DOTFILES_ROOT=$(dirname "$SCRIPT_DIR")
 
 # シンボリックリンクを作成する関数
 create_symlink() {
-  local link="$1"
-  local relative_target="$2"
-  local absolute_target="$DOTFILES_ROOT/$relative_target"
+  local source_rel="$1"
+  local source_abs="$DOTFILES_ROOT/$source_rel"
+  local target_dir="$2"
 
-  # リンク先のディレクトリが存在しない場合は作成する
-  local link_dir
-  link_dir=$(dirname "$link")
-  if [ ! -d "$link_dir" ]; then
-    mkdir -p "$link_dir"
-    echo "ディレクトリを作成しました: $link_dir"
+  # 作成するリンクのディレクトリが存在しない場合は作成する
+  if [ ! -d "$target_dir" ]; then
+    mkdir -p "$target_dir"
+    echo "ディレクトリを作成しました: $target_dir"
   fi
 
-  # シンボリックリンクが存在する場合は削除する
-  if [ -L "$link" ]; then
-    echo "既存のシンボリックリンクを削除します: $link"
-    rm "$link"
-  fi
-
-  # シンボリックリンクを作成する
-  if ln -s "$absolute_target" "$link"; then
-    echo "シンボリックリンクを作成しました: $link -> $absolute_target"
+  # シンボリックリンクを作成または更新する
+  if ln -sfv "$source_abs" "$target_dir"; then
+    :
   else
-    echo "シンボリックリンクの作成に失敗しました: $link -> $absolute_target"
+    echo "シンボリックリンクの作成または更新に失敗しました: '$target_dir/$source_rel' -> '$source_abs'"
   fi
 }
 
 # 設定に基づいてシンボリックリンクを作成する
-for link in "${!links[@]}"; do
-  create_symlink "$link" "${links[$link]}"
+for source in "${!links[@]}"; do
+  create_symlink "$source" "${links[$source]}"
 done
 
-echo "シンボリックリンクの作成が完了しました。"
+echo "シンボリックリンクの作成が完了しました"
