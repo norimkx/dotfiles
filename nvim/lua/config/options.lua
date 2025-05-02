dc873db1450035bb2a12d80e74c7ff916a1e9998
norimkx <norimkx@gmail.com>
2025-05-03 03:24:18 +0900

    fix: remove clipboard option

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,26 +3,3 @@
 -- Add any additional options here
 
 vim.opt.spelllang = { "en", "cjk" }
-
-if vim.fn.has("wsl") == 1 then
-  if vim.fn.executable("wl-copy") == 0 then
-    print("wl-clipboard not found, clipboard integration won't work")
-  else
-    vim.g.clipboard = {
-      name = "wl-clipboard (wsl)",
-      copy = {
-        ["+"] = "wl-copy --foreground --type text/plain",
-        ["*"] = "wl-copy --foreground --primary --type text/plain",
-      },
-      paste = {
-        ["+"] = function()
-          return vim.fn.systemlist("wl-paste --no-newline|sed -e 's/\r$//'", { "" }, 1)
-        end,
-        ["*"] = function()
-          return vim.fn.systemlist("wl-paste --primary --no-newline|sed -e 's/\r$//'", { "" }, 1)
-        end,
-      },
-      cache_enabled = true,
-    }
-  end
-end
