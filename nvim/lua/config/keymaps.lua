757f8297425160df23bca78390deb40271da9bb9
norimkx <norimkx@gmail.com>
2025-05-02 20:29:20 +0900

    fix: modify keymap to insert datetime

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,14 +4,14 @@
 
 vim.keymap.set(
   "n",
-  "<leader>cid",
+  "<leader>id",
   "i<C-r>=strftime('%Y-%m-%d %H:%M:%S')<CR><Esc>",
   { desc = "Insert Datetime", silent = true }
 )
 
 vim.keymap.set(
   "v",
-  "<leader>cid",
+  "<leader>id",
   "c<C-r>=strftime('%Y-%m-%d %H:%M:%S')<CR><Esc>",
   { desc = "Insert Datetime", silent = true }
 )
