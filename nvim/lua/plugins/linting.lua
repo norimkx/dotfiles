01ffaf637c41f685f2370b29633d9a0adaf58f4b
norimkx <norimkx@gmail.com>
2025-05-02 14:15:12 +0900

    feat: configure markdownlint-cli2

---
+++ nvim/lua/plugins/linting.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        markdown = { "markdownlint-cli2" },
+      },
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = {
+            "--config",
+            vim.fn.stdpath("config") .. "/.markdownlint-cli2.jsonc",
+            "--",
+          },
+        },
+      },
+    },
+  },
+}
