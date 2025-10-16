ad5a339535b1e545de44e492ca4af657bcf839d1
norimkx <norimkx@gmail.com>
2025-10-16 13:33:37 +0900

    chore: clean up config file comments

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,14 +1,9 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
 vim.keymap.set(
   "n",
   "<leader>id",
   "i<C-r>=strftime('%Y-%m-%d %H:%M:%S')<CR><Esc>",
   { desc = "Insert Datetime", silent = true }
 )
-
 vim.keymap.set(
   "v",
   "<leader>id",
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 vim.env.JAVA_HOME = "/home/linuxbrew/.linuxbrew/opt/openjdk"
 
 vim.opt.linebreak = false
