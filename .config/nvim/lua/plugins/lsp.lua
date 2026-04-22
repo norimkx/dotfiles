7664d1ce41ab3ccd81952b19e9c17a86b374348a
norimkx <norimkx@gmail.com>
2026-04-22 14:29:22 +0900

    feat(nvim): install powershell lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,16 @@ return {
   {
     "mason-org/mason.nvim",
     opts = {
-      ensure_installed = { "typos-lsp" },
+      ensure_installed = {
+        "powershell-editor-services",
+        "typos-lsp",
+      },
+    },
+  },
+  {
+    "TheLeoP/powershell.nvim",
+    opts = {
+      bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
     },
   },
 }
